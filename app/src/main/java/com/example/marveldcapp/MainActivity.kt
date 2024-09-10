08204403f04b0f9c24c2b9b3f2e6efc50a6f8f95
Yef666 <yef6rueda@outlook.com>
2024-09-09 22:37:10 -0600

    Initial commit

---
+++ app/src/main/java/com/example/marveldcapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.marveldcapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    fun onClickLogin(view: View) {
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
